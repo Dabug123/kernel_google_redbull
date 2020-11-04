7fda52366dfc54f405bb057bbf1df02ac1ed9aef
Dabug123 <clarkbrunswick55@gmail.com>
2020-11-04 18:42:44 +0100

    franken script

---
+++ franken.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+rm .version
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="Image"
+DTBIMAGE="dtb"
+export CLANG_PATH=~/android/clang11/clang-r399163/bin/
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=${HOME}/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=${HOME}/android/clang11/clang-r399163/lib64:$LD_LIBRARY_PATH
+DEFCONFIG="franken_defconfig"
+
+# Kernel Details
+VER=".pixel.b1"
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/android/AnyKernel3.1"
+PATCH_DIR="${HOME}/android/AnyKernel3.1/patch"
+ZIP_MOVE="${HOME}/android/pzip"
+ZIMAGE_DIR="${HOME}/android/p5/out/arch/arm64/boot/"
+
+# Functions
+function clean_all {
+		rm -rf ~/android/p5/out/*
+		cd $KERNEL_DIR
+		echo
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make CC=clang O=out $DEFCONFIG
+		make CC=clang O=out -j12
+
+}
+
+function make_dtb {
+		$REPACK_DIR/tools/dtbToolCM -2 -o $REPACK_DIR/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm64/boot/
+}
+
+function make_boot {
+		cp -vr ~/android/p5/out/arch/arm64/boot/Image.gz-dtb ~/android/AnyKernel3.1/Image.gz-dtb
+}
+
+
+function make_zip {
+		cd ~/android/AnyKernel3.1
+		zip -r9 `echo $AK_VER`.zip *
+		mv  `echo $AK_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+
+DATE_START=$(date +"%s")
+
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+
+# Vars
+BASE_AK_VER="FrankenKernel"
+AK_VER="$BASE_AK_VER$VER"
+export LOCALVERSION=~`echo $AK_VER`
+export LOCALVERSION=~`echo $AK_VER`
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=Dabug123
+export KBUILD_BUILD_HOST=TheLab
+
+echo
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build?" dchoice
+do
+case "$dchoice" in
+	y|Y )
+		make_kernel
+		make_boot
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
